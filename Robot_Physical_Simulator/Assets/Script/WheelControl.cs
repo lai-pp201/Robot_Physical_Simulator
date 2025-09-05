be28f6eff590b1261bfd6f7bbfdddaf2d0b10934
lai-pp201 <lyhhowardlai@gmail.com>
2025-09-05 15:07:04 +0800

    Second

--- Robot_Physical_Simulator/Assets/Script/WheelControl.cs
+++ Robot_Physical_Simulator/Assets/Script/WheelControl.cs
@@ -8,6 +8,10 @@ public class WheelControl : MonoBehaviour
 
     [SerializeField] private WheelCollider _wheelLeft;
     [SerializeField] private WheelCollider _wheelRight;
+    public Transform leftWheelMesh;
+    public Transform rightWheelMesh;
+    float leftMotor;
+    float rightMotor;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +21,44 @@ public class WheelControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float leftMotor = (Input.GetKey(KeyCode.A)) ? _motorSpeed : 0;
-        float rightMotor = (Input.GetKey(KeyCode.D)) ? _motorSpeed : 0;
-        _wheelLeft.motorTorque = leftMotor;
+        if (Input.GetKey(KeyCode.W))
+        {
+            leftMotor = _motorSpeed;
+            rightMotor = _motorSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            leftMotor = -_motorSpeed;
+            rightMotor = -_motorSpeed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            leftMotor = -_motorSpeed;
+            rightMotor = _motorSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            leftMotor = _motorSpeed;
+            rightMotor = -_motorSpeed;
+        }
+        else
+        {
+            leftMotor = 0;
+            rightMotor = 0;
+        }
+            //float leftMotor = (Input.GetKey(KeyCode.A)) ? _motorSpeed : 0;
+            //float rightMotor = (Input.GetKey(KeyCode.D)) ? _motorSpeed : 0;
+
+            _wheelLeft.motorTorque = leftMotor;
         _wheelRight.motorTorque = rightMotor;
+
+        //_wheelLeft.GetWorldPose(out Vector3 posL, out Quaternion rotL);
+        //leftWheelMesh.position = posL;
+        //leftWheelMesh.rotation = rotL;
+
+        //_wheelRight.GetWorldPose(out Vector3 posR, out Quaternion rotR);
+        //rightWheelMesh.position = posL;
+        //rightWheelMesh.rotation = rotL;
+
     }
 }
